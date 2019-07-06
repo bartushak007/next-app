ba52f50adec1d51863636278f3d8c91982a4eb4f
Roman <rmnbrtshk@gmail.com>
2019-07-06 21:00:41 +0300

    Deploy gh-pages

---
+++ _next/static/development/pages/ProductList.js
@@ -0,0 +1,89 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static\\development\\pages\\ProductList.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2FProductList&absolutePagePath=C%3A%5CUsers%5Crmnbr%5COneDrive%5C%D0%A0%D0%BE%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D1%96%D0%BB%5Ctestfolder%5Cnext-app%5Cpages%5CProductList.js!./":
+/*!********************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2FProductList&absolutePagePath=C%3A%5CUsers%5Crmnbr%5COneDrive%5C%D0%A0%D0%BE%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D1%96%D0%BB%5Ctestfolder%5Cnext-app%5Cpages%5CProductList.js ***!
+  \********************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/ProductList", function() {
+      var page = __webpack_require__(/*! ./pages/ProductList.js */ "./pages/ProductList.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/ProductList.js */ "./pages/ProductList.js", function() {
+          if(!next.router.components["/ProductList"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/ProductList.js */ "./pages/ProductList.js")
+          next.router.update("/ProductList", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_6dc2816e14fab51b8269 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_6dc2816e14fab51b8269 */ "dll-reference dll_6dc2816e14fab51b8269"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/ProductList.js":
+/*!******************************!*\
+  !*** ./pages/ProductList.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "C:\\Users\\rmnbr\\OneDrive\\\u0420\u043E\u0431\u043E\u0447\u0438\u0439 \u0441\u0442\u0456\u043B\\testfolder\\next-app\\pages\\ProductList.js";
+
+
+var ProductsList = function ProductsList(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "ProductsList", props.url && props.url.query.title);
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (ProductsList);
+
+/***/ }),
+
+/***/ 2:
+/*!************************************************************************************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2FProductList&absolutePagePath=C%3A%5CUsers%5Crmnbr%5COneDrive%5C%D0%A0%D0%BE%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D1%96%D0%BB%5Ctestfolder%5Cnext-app%5Cpages%5CProductList.js ***!
+  \************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2FProductList&absolutePagePath=C%3A%5CUsers%5Crmnbr%5COneDrive%5C%D0%A0%D0%BE%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D1%96%D0%BB%5Ctestfolder%5Cnext-app%5Cpages%5CProductList.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2FProductList&absolutePagePath=C%3A%5CUsers%5Crmnbr%5COneDrive%5C%D0%A0%D0%BE%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D1%96%D0%BB%5Ctestfolder%5Cnext-app%5Cpages%5CProductList.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_6dc2816e14fab51b8269":
+/*!*******************************************!*\
+  !*** external "dll_6dc2816e14fab51b8269" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_6dc2816e14fab51b8269;
+
+/***/ })
+
+},[[2,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=ProductList.js.map
---
+++ _next/static/webpack/static/development/pages/index.js.996a635277867d938ddf.hot-update.js
@@ -0,0 +1,32 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./components/ProductList.js":
+/*!***********************************!*\
+  !*** ./components/ProductList.js ***!
+  \***********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "C:\\Users\\rmnbr\\OneDrive\\\u0420\u043E\u0431\u043E\u0447\u0438\u0439 \u0441\u0442\u0456\u043B\\testfolder\\next-app\\components\\ProductList.js";
+
+
+var ProductsList = function ProductsList() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "ProductsList");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (ProductsList);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.996a635277867d938ddf.hot-update.js.map
